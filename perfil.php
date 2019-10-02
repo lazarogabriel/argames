e81f21ea91de845eafb351ac77062a19078c2683
lazarogabriel <lazarogabriel2000@gmail.com>
2019-10-02 00:28:30 -0300

    comit miercoles 2

--- perfil.php
+++ perfil.php
@@ -124,6 +124,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/reset.css">
 		<link rel="stylesheet" href="css/bootstrap.css">
